02b20ffd971e448075d635c6e53e5a6a8b0d633d
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-11-10 20:24:51 +0200

    module3 task5.

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        TimeConvertor a = new TimeConvertor();
+        convert(minutes*60);
     }
 }
